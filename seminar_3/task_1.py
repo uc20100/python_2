30e35085c849c87b6ab55213aa18559b0280d22c
Evgeny Egipti <uc20100@gmail.com>
2023-09-10 21:42:18 +0400

    Подправил внешний вид

--- seminar_3/task_1.py
+++ seminar_3/task_1.py
@@ -5,21 +5,21 @@
 # ✔ Текст выравнивается по правому краю так, чтобы у самого длинного
 # слова был один пробел между ним и номером строки.
 
-# STR_INDEX = 1
-# UTF8_INDEX = 0
-#
-# user_word = input('Введите строку текста: ').split()
-# user_word_utf_8 = [i.encode('UTF-8') for i in user_word]
-# list_str_utf8 = list(zip(user_word_utf_8, user_word))
-# list_str_utf8.sort()
-# max_len_word = 0
-# for item in list_str_utf8:
-#     if max_len_word < len(item[STR_INDEX]):
-#         max_len_word = len(item[STR_INDEX])
-# for i, item in enumerate(list_str_utf8, 1):
-#     print(f'{i} {item[STR_INDEX]: >{max_len_word}}')
+STR_INDEX = 1
+UTF8_INDEX = 0
 
+user_word = input('Введите строку текста: ').split()
+user_word_utf_8 = [i.encode('UTF-8') for i in user_word]
+list_str_utf8 = list(zip(user_word_utf_8, user_word))
+list_str_utf8.sort()
+max_len_word = 0
+for item in list_str_utf8:
+    if max_len_word < len(item[STR_INDEX]):
+        max_len_word = len(item[STR_INDEX])
+for i, item in enumerate(list_str_utf8, 1):
+    print(f'{i} {item[STR_INDEX]: >{max_len_word}}')
 
+print()
 # Задание №7
 # ✔ Пользователь вводит строку текста.
 # ✔ Подсчитайте сколько раз встречается
@@ -32,16 +32,16 @@
 # Объясните почему они совпадают
 # или не совпадают в ваших решениях.
 
-# user_str = input('Введите строку текста: ')
-# user_dict = {}
-# for item in user_str:
-#     if not user_dict.get(item):
-#         user_dict[item] = 1
-#     else:
-#         user_dict[item] += 1
-# print(user_dict)
+user_str = input('Введите строку текста: ')
+user_dict = {}
+for item in user_str:
+    if not user_dict.get(item):
+        user_dict[item] = 1
+    else:
+        user_dict[item] += 1
+print(user_dict)
 
-
+print('')
 # Задание №8
 # ✔ Три друга взяли вещи в поход. Сформируйте
 # словарь, где ключ — имя друга, а значение —
@@ -79,7 +79,6 @@ for i in hike:
     unique_things = unique_things | (one_things - other_things)
     other_things.clear()
 print(f'Уникальные вещи: {unique_things}')
-
 
 one_things = None
 other_things = {1, 2}
