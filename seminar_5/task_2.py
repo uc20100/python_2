5e1203cb8dac047e2d9dedf639f66ac7aed5a332
Evgeny Egipti <uc20100@gmail.com>
2023-09-15 20:50:11 +0400

    Все переделал

--- seminar_5/task_2.py
+++ seminar_5/task_2.py
@@ -5,21 +5,10 @@
 # премии в качестве значения. Сумма рассчитывается
 # как ставка умноженная на процент премии
 
-def bonuses(*, name: list, rate: list, percent: list) -> dict:
-    """
-    Премии сотрудников.
-
-    :param name: имя
-    :param rate: ставка
-    :param percent: процентная ставка
-    :return: ведомость премий
-    """
-    yield {name_: rate_ * float(percent_[:-1])*0.01 for name_, rate_, percent_ in list(zip(name, rate, percent))}
-
-
 name_user = ['Иванов', 'Петров', 'Сидоров']
 rate_user = [100, 200, 300]
 percent_user = ['10.5%', '5.5%', '6.1%']
 
-for item in bonuses(name=name_user, rate=rate_user, percent=percent_user):
-    print(item)
+bonuse = {name_: rate_ * float(percent_[:-1])*0.01 for name_, rate_, percent_ in list(zip(name_user, rate_user, percent_user))}
+
+print(bonuse)
