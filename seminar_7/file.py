777409a6aad929578d4c29cdf061a4bf7525d179
Evgeny Egipti <uc20100@gmail.com>
2023-09-29 23:38:35 +0400

    Выполнил задание №2

--- seminar_7/file.py
+++ seminar_7/file.py
@@ -8,9 +8,10 @@
 import random as rnd
 
 
-def write_random_number(name: str, lines: int):
+def random_number(name: str, lines: int):
     """
     Функция заполняет файл случайными числами
+
 
     :param name: имя файла
     :param lines: количество строк
@@ -21,5 +22,36 @@ def write_random_number(name: str, lines: int):
             print(f'{rnd.randint(-1_000, 1000)} | {round(rnd.uniform(-1_000, 1_000), 2)}', file=f)
 
 
+# Задание №2
+# ✔ Напишите функцию, которая генерирует
+# псевдоимена.
+# ✔ Имя должно начинаться с заглавной буквы,
+# состоять из 4-7 букв, среди которых
+# обязательно должны быть гласные.
+# ✔ Полученные имена сохраните в файл.
+
+def pseudonyms(name: str, lines):
+    """
+    Функция генерит псевдоимена.
+
+    :return:
+    """
+    vowels = ('а', 'е', 'и', 'о', 'э', 'ю', 'я')
+    count = 0
+
+    with open(f'{name}.txt', 'w', encoding='utf-8') as f:
+        while count < lines:
+            result_str = ''
+            for _ in range(8):
+                result_str += chr(rnd.randint(0x430, 0x44f))
+            for item in vowels:
+                if item in result_str:
+                    result_str = result_str[0].upper() + result_str[1::]
+                    print(result_str, file=f)
+                    count += 1
+                    break
+
+
 if __name__ == '__main__':
-    write_random_number('example', 10)
+    # random_number('example', 10)
+    pseudonyms('example', 10)
