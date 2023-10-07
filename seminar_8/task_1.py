b4d6957a4f51dae5502a4803ef05534d34c961bf
Evgeny Egipti <uc20100@gmail.com>
2023-10-07 16:25:05 +0400

    Выполнил задание №1

---
+++ seminar_8/task_1.py
@@ -0,0 +1,47 @@
+# * Задание №1
+#   - Вспоминаем задачу 3 из прошлого семинара. Мы сформировали
+# текстовый файл с псевдо именами и произведением чисел.
+#   - Напишите функцию, которая создаёт из созданного ранее
+# файла новый с данными в формате JSON.
+#   - Имена пишите с большой буквы.
+#   - Каждую пару сохраняйте с новой строки.
+
+from file_func.task_1 import random_number
+from file_func.task_2 import pseudonyms
+from file_func.task_3 import multiplication
+import json
+
+
+def multiplication_json(in_file: str, out_file: str):
+    """
+    Функция создает файл в формате JSON из текстового файла
+
+    :param in_file: файл в текстовом формате
+    :param out_file: файл в формате JSON
+    :return: 
+    """
+    json_dict = {}
+    with(open(in_file, 'r', encoding='utf-8') as f_in,
+         open(out_file, 'w', encoding='utf-8') as f_out):
+        while read_str := f_in.readline():
+            key, value = read_str.split()
+            if value.isdigit():
+                value = int(value)
+            else:
+                value = float(value)
+            json_dict[key.upper()] = value
+        json.dump(json_dict, f_out, ensure_ascii=False, separators=(',\n', ':'), )
+
+
+# Формируем текстовый файл с псевдо именами и произведением чисел (из прошлого семинара)
+random_number('numbers.txt', 8)
+pseudonyms('pseudonyms.txt', 5)
+multiplication('numbers.txt', 'pseudonyms.txt', 'multiplication.txt')
+# Формируем файл JSON
+multiplication_json('multiplication.txt', 'multiplication_json.txt')
+
+
+if __name__ == '__main__':
+    with open('multiplication_json.txt', 'r', encoding='utf-8') as f:
+        new_dict = json.load(f)
+    print(f'{new_dict = }')
