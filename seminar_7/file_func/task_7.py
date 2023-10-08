bdac12c3113413f9ac425677d30e4e0043a52eba
Evgeny Egipti <uc20100@gmail.com>
2023-10-08 22:16:24 +0400

    Забыл удалить print()

--- seminar_7/file_func/task_7.py
+++ seminar_7/file_func/task_7.py
@@ -32,7 +32,6 @@ def sort_file(sort_folder: str, **kwargs):
     for obj in p.iterdir():
         if obj.is_file():
             for type_folder, list_type_file in kwargs.items():
-                print(obj)
                 *_, file_name_all = str(obj).split('\\')
                 name_file, type_ = str(file_name_all).split('.')
                 if type_ in list_type_file:
