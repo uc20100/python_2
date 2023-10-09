4647657c7e7ae795511552290c58dd768c41a363
Evgeny Egipti <uc20100@gmail.com>
2023-10-09 18:57:52 +0400

    Исправил размещение каталогов

--- seminar_8/file_func/task_7.py
+++ seminar_8/file_func/task_7.py
@@ -7,6 +7,7 @@
 # ✔ В исходной папке должны остаться только те файлы, которые не подошли для сортировки.
 
 __all__ = ['sort_file']
+
 from pathlib import Path
 import shutil
 
@@ -20,22 +21,21 @@ def sort_file(sort_folder: str, **kwargs):
     :return:
     """
 
-    p = Path(Path().cwd())
+    p = Path(Path.cwd() / sort_folder)
     for folder in kwargs.keys():
         for obj in p.iterdir():
-            if obj == Path().cwd() / folder and obj.is_dir():
+            if obj == p / folder and obj.is_dir():
                 break
         else:
-            Path(folder).mkdir()
+            Path(p / folder).mkdir()
 
-    p = Path(Path.cwd() / sort_folder)
     for obj in p.iterdir():
         if obj.is_file():
             for type_folder, list_type_file in kwargs.items():
                 *_, file_name_all = str(obj).split('\\')
                 name_file, type_ = str(file_name_all).split('.')
                 if type_ in list_type_file:
-                    obj.replace(Path.cwd() / f'{type_folder}' / f'{name_file}.{type_}')
+                    obj.replace(p / f'{type_folder}' / f'{name_file}.{type_}')
 
 
 if __name__ == '__main__':
