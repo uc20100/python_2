37179d2d56d0923ae435312bf0e10d4a58711d48
Evgeny Egipti <uc20100@gmail.com>
2023-10-09 10:09:37 +0400

    Переделал проверку функции

--- seminar_8/task_5.py
+++ seminar_8/task_5.py
@@ -7,9 +7,11 @@
 # одноимённых pickle файлов.
 
 __all__ = ['json_to_pickle']
+
 import json
 from pathlib import Path
-from file_func.task_7 import sort_file
+if __name__ == '__main__':
+    import shutil
 import pickle
 
 
@@ -25,16 +27,28 @@ def json_to_pickle(folder: str):
         if obj.is_file():
             *_, name_file_all = str(obj).split('\\')
             name_file, type_file = name_file_all.split('.')
-            with (open(p / name_file_all, 'r', encoding='utf-8') as f_read,
-                  open(p / f'{name_file}.pickle', 'wb') as f_write):
-                read_json = json.load(f_read)
-                pickle.dump(read_json, f_write)
+            if type_file == 'json':
+                with (open(p / name_file_all, 'r', encoding='utf-8') as f_read,
+                      open(p / f'{name_file}.pickle', 'wb') as f_write):
+                    read_json = json.load(f_read)
+                    pickle.dump(read_json, f_write)
 
 
 if __name__ == '__main__':
-    # будем перемещать файлы json в новую папку
-    folder_dict = dict(test_folder=['json'])
-    sort_file('', **folder_dict)
+    # Удаляем папку если она есть и снова её создаём
+    folder_ = 'test_folder'
+    p = Path(Path().cwd())
+    for obj in p.iterdir():
+        if obj == Path().cwd() / folder_ and obj.is_dir():
+            shutil.rmtree(folder_)
+            break
+    Path(folder_).mkdir()
+    # Копируем файлы JSON из текущей директории в новую
+    for obj in p.iterdir():
+        if obj.is_file():
+            *_, name_file_all = str(obj).split('\\')
+            *_, type_file = name_file_all.split('.')
+            if type_file == 'json':
+                shutil.copy(name_file_all, folder_)
     # преобразовываем json в pickle
-    json_to_pickle('test_folder')
-
+    json_to_pickle(folder_)
