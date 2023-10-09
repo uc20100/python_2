1f8414a11761788983f4739c930323b94fe1002b
Evgeny Egipti <uc20100@gmail.com>
2023-10-09 08:09:33 +0400

    Улучшил код

--- seminar_8/task_1.py
+++ seminar_8/task_1.py
@@ -10,9 +10,10 @@
 #   - Каждую пару сохраняйте с новой строки.
 
 __all__ = ['multiplication_json']
-from file_func.task_1 import random_number
-from file_func.task_2 import pseudonyms
-from file_func.task_3 import multiplication
+if __name__ == '__main__':
+    from file_func.task_1 import random_number
+    from file_func.task_2 import pseudonyms
+    from file_func.task_3 import multiplication
 import json
 
 
@@ -37,15 +38,14 @@ def multiplication_json(in_file: str, out_file: str):
         json.dump(json_dict, f_out, ensure_ascii=False, separators=(',\n', ':'), )
 
 
-# Формируем текстовый файл с псевдо именами и произведением чисел (из прошлого семинара)
-random_number('numbers.txt', 8)
-pseudonyms('pseudonyms.txt', 5)
-multiplication('numbers.txt', 'pseudonyms.txt', 'multiplication.txt')
-# Формируем файл JSON
-multiplication_json('multiplication.txt', 'multiplication.json')
-
-
 if __name__ == '__main__':
+    # Формируем текстовый файл с псевдо именами и произведением чисел (из прошлого семинара)
+    random_number('numbers.txt', 8)
+    pseudonyms('pseudonyms.txt', 5)
+    multiplication('numbers.txt', 'pseudonyms.txt', 'multiplication.txt')
+    # Формируем файл JSON
+    multiplication_json('multiplication.txt', 'multiplication.json')
+
     with open('multiplication.json', 'r', encoding='utf-8') as f:
         new_dict = json.load(f)
     print(f'{new_dict = }')
