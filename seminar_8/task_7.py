b06db3a4cf7399f873d9ccd0d91b7c4e80cc0a44
Evgeny Egipti <uc20100@gmail.com>
2023-10-09 16:49:18 +0400

    Выполнил задание №7

---
+++ seminar_8/task_7.py
@@ -0,0 +1,45 @@
+"""
+Модуль чтения CSV файла
+"""
+
+# Задание №7
+# 📌 Прочитайте созданный в прошлом задании csv файл без
+# использования csv.DictReader.
+# 📌 Распечатайте его как pickle строку.
+
+__all__ = ['read_csv']
+import pickle
+
+
+def read_csv(file_csv: str) -> tuple[bytes, bytes]:
+    """
+    Функция читает CSV файл и распечатывает как pickle строку
+
+    :param file_csv: CSV файл
+    :return: байтовая строка [0] - CSV файл, [1] - словарь
+    """
+    result_str = ''
+    with open(file_csv, 'r', encoding='utf-8') as f_read:
+        for row in f_read:
+            result_str += row
+    str_bytes = pickle.dumps(result_str)
+
+    result_list = []
+    with open(file_csv, 'r', encoding='utf-8') as f_read_:
+        for i, row_ in enumerate(f_read_, 0):
+            if i == 0:
+                key_1, key_2, key_3, key_4, key_5 = row_[:-1].split(',')
+            else:
+                val_1, val_2, val_3, val_4, val_5 = row_[:-1].split(',')
+                result_list.append({key_1: val_1, key_2: val_2, key_3: val_3, key_4: val_4, key_5: val_5})
+    dict_bytes = pickle.dumps(result_list)
+
+    return str_bytes, dict_bytes
+
+
+if __name__ == '__main__':
+    list_bytes = read_csv('test_folder\\user_upgrade.csv')
+    print(pickle.loads(list_bytes[0]))
+    print()
+    print(pickle.loads(list_bytes[1]))
+
