cb38939431ea4dafbb55e48640b4de8916915d43
Evgeny Egipti <uc20100@gmail.com>
2023-10-16 23:37:54 +0400

    Добавил модуль ДЗ

--- seminar_9/decorator/__init__.py
+++ seminar_9/decorator/__init__.py
@@ -1,4 +1,4 @@
 """
 Пакет функций с декоратором
 """
-__all__ = ['task_1', 'task_2', 'task_3', 'task_4', 'task_5', 'task_6']
+__all__ = ['task_1', 'task_2', 'task_3', 'task_4', 'task_5', 'task_6', 'task_home_work']
