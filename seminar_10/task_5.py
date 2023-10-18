047972bb537ac53288d58fafcd8406a38ccad5b0
Evgeny Egipti <uc20100@gmail.com>
2023-10-19 01:26:07 +0400

    Выполнил задание 5

---
+++ seminar_10/task_5.py
@@ -0,0 +1,46 @@
+# Задание №5
+# 📌 Создайте три (или более) отдельных классов животных.
+# Например рыбы, птицы и т.п.
+# 📌 У каждого класса должны быть как общие свойства,
+# например имя, так и специфичные для класса.
+# 📌 Для каждого класса создайте метод, выводящий
+# информацию специфичную для данного класса.
+
+class Birds:
+    def __init__(self):
+        self.planet = 'Earth'
+        self.habitat_birds = 'air'
+
+    def view_habitat_birds(self):
+        print(self.habitat_birds)
+
+
+class Fish:
+    def __init__(self):
+        self.planet = 'Earth'
+        self.habitat_fish = 'water'
+
+    def view_habitat_fish(self):
+        print(self.habitat_fish)
+
+
+class Human:
+    def __init__(self):
+        self.planet = 'Earth'
+        self.habitat_human = 'ground'
+
+    def view_habitat_human(self):
+        print(self.habitat_human)
+
+
+if __name__ == '__main__':
+    birds = Birds()
+    fish = Fish()
+    human = Human()
+
+    print(f'{birds.planet = }')
+    birds.view_habitat_birds()
+    print(f'{fish.planet = }')
+    fish.view_habitat_fish()
+    print(f'{human.planet = }')
+    human.view_habitat_human()
