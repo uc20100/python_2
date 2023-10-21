6da633d2d315a158d35c9f39cbd045a1fac70f3d
Evgeny Egipti <uc20100@gmail.com>
2023-10-21 21:32:11 +0400

    Выполнил задание 1

---
+++ seminar_11/task_1.py
@@ -0,0 +1,20 @@
+# Задание №1
+# 📌 Создайте класс Моя Строка, где:
+# 📌 будут доступны все возможности str
+# 📌 дополнительно хранятся имя автора строки и время создания
+# (time.time)
+
+import time
+
+
+class MyStr(str):
+    def __new__(cls, value, author):
+        instance = super().__new__(cls, value)
+        instance.author = author
+        instance.time = time.time()
+        return instance
+
+
+if __name__ == '__main__':
+    one_str = MyStr('Hello world!', 'Evgeny Egipti')
+    print(f'{one_str.upper() = }, {one_str.author = }, {one_str.time = }')
