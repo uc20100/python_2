36c6eddbee531b720f24a804f1954cee9325f2fe
Evgeny Egipti <uc20100@gmail.com>
2023-10-31 23:36:53 +0400

    Добавил описание классов и функций

--- seminar_13/task_3.py
+++ seminar_13/task_3.py
@@ -9,6 +9,16 @@ class UsException(BaseException):
 
 
 class LevelException(UsException):
+    """
+        Класс ошибки доступа пользователя.
+
+         Атрибуты:
+        - self.old_level (int | str): старый уровень доступа пользователя,
+        - self.new_level (int | str): новый уровень доступа пользователя.
+
+         Dunder методы:
+        - __str__(self): Возвращает строковое представление ошибки.
+        """
     def __init__(self, old_level, new_level):
         self.old_level = old_level
         self.new_level = new_level
@@ -17,8 +27,18 @@ class LevelException(UsException):
         return f"Ваш уровень '{self.new_level}' меньше уровня '{self.old_level}', а должен быть больше или равен"
 
 
-
 class AccessException(UsException):
+    """
+    Класс ошибки доступа пользователя.
+
+     Атрибуты:
+    - self.name (str): имя пользователя,
+    - self.identifier (str): id пользователя.
+
+     Dunder методы:
+    - __str__(self): Возвращает строковое представление ошибки.
+    """
+
     def __init__(self, name, identifier):
         self.name = name
         self.identifier = identifier
