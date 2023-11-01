4bf8224cbb8b0b712ae1ddb8db35e031516bef53
Evgeny Egipti <uc20100@gmail.com>
2023-11-01 11:33:40 +0400

    Добавил даандер методы сравнения и хеширования

--- seminar_13/task_4.py
+++ seminar_13/task_4.py
@@ -86,14 +86,26 @@ class Us:
     - self.name (str): имя пользователя,
     - self.identifier (str): id пользователя,
     - self.level (str): уровень доступа пользователя.
+
+     Dunder методы:
+    - def __init__(self, name, identifier, level): конструктор класса,
+    - __eq__(self, other): сравнение двух экземпляров класса,
+    - __hash__(self): возвращает собственный hash,
+    - __str__(self): возвращает строковое представление множества.
     """
     def __init__(self, name, identifier, level):
         self.name = name
         self.identifier = identifier
         self.level = level
 
+    def __eq__(self, other):
+        return self.identifier == other.identifier and self.name == other.name
+
     def __str__(self):
         return f'Идентификатор: {self.identifier}, Имя: {self.name}, Уровень: {self.level}'
+
+    def __hash__(self):
+        return hash((self.name, self.identifier, self.level))
 
 
 if __name__ == '__main__':
