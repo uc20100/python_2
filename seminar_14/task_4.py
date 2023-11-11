b8a44812e4626e31e4f1086275d6d4f678819c93
Evgeny Egipti <uc20100@gmail.com>
2023-11-11 14:48:46 +0400

    Исправил задание 3

--- seminar_14/task_4.py
+++ seminar_14/task_4.py
@@ -14,19 +14,19 @@ from task_1 import modification_str
 
 
 def test_without_changes():
-    assert modification_str('Hello World, Привет мир!') == 'Hello World, Привет мир!', 'Тест1 провален'
+    assert modification_str('hello world') == 'hello world', 'Тест1 провален'
 
 
 def test_small_case():
-    assert modification_str('Hello World, Привет мир!') == 'hello world, привет мир!', 'Тест2 провален'
+    assert modification_str('Hello World') == 'hello world', 'Тест2 провален'
 
 
 def test_no_punctuation():
-    assert modification_str('Hello World, Привет мир!') == 'Hello World Привет мир', 'Тест3 провален'
+    assert modification_str('hello world!') == 'hello world', 'Тест3 провален'
 
 
 def test_only_latin():
-    assert modification_str('Hello World, Привет мир!') == 'Hello World,  !', 'Тест4 провален'
+    assert modification_str('hello world_Привет_мир') == 'hello world', 'Тест4 провален'
 
 
 def test_all_conditions():
