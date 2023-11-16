cf9f421d7575746d0f5d1a01d4b39571162457a0
Evgeny Egipti <uc20100@gmail.com>
2023-11-16 21:31:05 +0400

    Добавил описание командной строки

--- seminar_15/task_5.py
+++ seminar_15/task_5.py
@@ -115,4 +115,8 @@ if __name__ == '__main__':
                         help='месяц года (январь-декабрь или 1-12)', default=datetime.now().month)
     args = parser.parse_args()
     print(f"-week:{args.week}, -weekday:{args.weekday}, -month:{args.month}, result_day:{convert_text_to_date(f'{args.week} {args.weekday} {args.month}')}")
+
+    # Запуск из командной строки
     # python task_5.py -week 1 -weekday 1 -month 1
+    # Описание параметров
+    # python task_5.py --help
